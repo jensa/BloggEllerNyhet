18a8c3f529a3e69ff35b474b0d91eb2ba80a6d3b
Victor Hallberg <vigge19@gmail.com>
2012-12-06 13:47:57 +0100

    Use stopwords for TextCategorizationTest

--- src/TextCategorizationTest.java
+++ src/TextCategorizationTest.java
@@ -24,22 +24,20 @@ public class TextCategorizationTest {
    * @throws Exception  if something goes wrong
    */
   public static void main(String[] args) throws Exception {
-    // convert the directory into a dataset
+    // Convert the directory into a dataset
     TextDirectoryLoader loader = new TextDirectoryLoader();
     loader.setDirectory(new File(args[0]));
     Instances dataRaw = loader.getDataSet();
     //System.out.println("\n\nImported data:\n\n" + dataRaw);
 
-    // apply the StringToWordVector
-    // (see the source code of setOptions(String[]) method of the filter
-    // if you want to know which command-line option corresponds to which
-    // bean property)
+    // Apply the StringToWordVector
     StringToWordVector filter = new StringToWordVector();
     filter.setInputFormat(dataRaw);
+    filter.setOptions(new String[] { "-stopwords", "stopwords.txt" });
     Instances dataFiltered = Filter.useFilter(dataRaw, filter);
     //System.out.println("\n\nFiltered data:\n\n" + dataFiltered);
 
-    // train J48 and output model
+    // Train classifier and output model
     J48 classifier = new J48();
     classifier.buildClassifier(dataFiltered);
     System.out.println("\n\nClassifier model:\n\n" + classifier);
